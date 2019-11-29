9fb8c0f00e37dcd22d5ada5e4d0439bf59865e0e
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-11-29 10:11:20 +0000

    Scripted auto-commit on change (2019-11-29 10:11:20) by gitwatch.sh

--- src/main/java/StressTestingApp.java
+++ src/main/java/StressTestingApp.java
@@ -1,2 +1,37 @@
+import akka.NotUsed;
+import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+
+import java.io.IOException;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.CompletionStage;
+
+import static jdk.nashorn.internal.parser.TokenType.RETURN;
+
 public class StressTestingApp {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("start!");
+        ActorSystem system = ActorSystem.create("routes");
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer =
+                ActorMaterializer.create(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = <вызов метода которому передаем Http, ActorSystem и ActorMaterializer>;
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate()); // and shutdown
+        when done
+    }
 }
