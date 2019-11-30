df81b8d1ef735156dd3c9be9ef1161cf0e61004a
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-11-30 09:01:57 +0000

    Scripted auto-commit on change (2019-11-30 09:01:57) by gitwatch.sh

--- src/main/java/ActorSystem.java
+++ src/main/java/ActorSystem.java
@@ -1,2 +1,9 @@
-public class ActorSystem {
+import akka.actor.AbstractActor;
+
+import java.util.Map;
+
+public class ActorSystem extends AbstractActor {
+
+    private Map<String, Long> = newHashMap<>();
+
 }
