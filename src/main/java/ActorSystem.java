05f593b7bcb310fe1ea855cc669cbd3d5e35e719
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-11-30 11:27:10 +0000

    Scripted auto-commit on change (2019-11-30 11:27:10) by gitwatch.sh

--- src/main/java/ActorSystem.java
+++ src/main/java/ActorSystem.java
@@ -1,4 +1,5 @@
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -6,6 +7,11 @@ import java.util.Map;
 public class ActorSystem extends AbstractActor {
 
     private Map<String, Long> keeper = new HashMap<>();
-    
+
+    @Override
+    public Receive createReceive() {
+        return ReceiveBuilder.create()
+                .match(Request.class, (req) ->)
+    }
 
 }
