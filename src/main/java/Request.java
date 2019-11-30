1509a00a86e8e7cd8615fba7dd2d0b33153c7745
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-11-30 11:29:35 +0000

    Scripted auto-commit on change (2019-11-30 11:29:35) by gitwatch.sh

--- src/main/java/Request.java
+++ src/main/java/Request.java
@@ -1,2 +1,26 @@
 public class Request {
+
+    private String url;
+    private int index;
+
+    public Request(String url, int index) {
+        this.url = url;
+        this.index = index;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
