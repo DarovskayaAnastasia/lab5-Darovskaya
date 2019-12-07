eb3ab384a9cf0fa6be73cb7ab18545a4ec3a5992
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-12-07 06:52:09 +0000

    Scripted auto-commit on change (2019-12-07 06:52:09) by gitwatch.sh

--- src/main/java/StressTestingApp.java
+++ src/main/java/StressTestingApp.java
@@ -46,7 +46,8 @@ public class StressTestingApp {
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
 
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = (http, system, materializer)<вызов метода которому передаем Http, ActorSystem и ActorMaterializer>;
+        final PingServer server = new PingServer(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = server.
 
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
